33f5e088643fb8fd3a3ebbcced5d3eff87fb4dea
TSekita <tatsuyukisekita749@gmail.com>
2025-04-07 02:48:58 +0000

    202504071148

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 uint8_t set_bit(uint8_t val, int bit) {
     uint8_t new_val = val;
-    new_val |= (1 << bit - 1);
+    new_val |= (1 << bit);
     return new_val;
 }
 
 uint8_t clear_bit(uint8_t val, int bit) {
     uint8_t new_val = val;
-    new_val &= ~(1 << bit - 1);
+    new_val &= ~(1 << bit);
     return new_val;
 }
 
@@ -20,12 +20,12 @@ void print_binary(uint8_t val) {
 }
 
 int main(void) {
-    for (int i = 1; i <= 8; i++) {
+    for (int i = 0; i < 8; i++) {
         print_binary(set_bit(0,i));
         printf("\n");
     }
     printf("\n");
-    for (int j = 1; j <= 8; j++) {
+    for (int j = 0; j < 8; j++) {
         print_binary(clear_bit(255,j));
         printf("\n");
     }
